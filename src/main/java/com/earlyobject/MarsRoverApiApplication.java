7b4e0782984f2dbe0fc2553b23552e7d650d697f
EarlyObject <sr.ardak@gmail.com>
2022-04-04 13:34:25 +0200

    First steps

---
+++ src/main/java/com/earlyobject/MarsRoverApiApplication.java
@@ -0,0 +1,14 @@
+package com.earlyobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MarsRoverApiApplication.class, args);
+    }
+
+}
