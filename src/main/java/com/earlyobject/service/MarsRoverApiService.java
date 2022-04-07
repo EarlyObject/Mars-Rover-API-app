5e0b36d1c4234427497a421a30af3288a6040b5d
EarlyObject <sr.ardak@gmail.com>
2022-04-07 10:57:42 +0200

    changing POST form to GET form, introducing API_KEY

--- src/main/java/com/earlyobject/service/MarsRoverApiService.java
+++ src/main/java/com/earlyobject/service/MarsRoverApiService.java
@@ -8,12 +8,15 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class MarsRoverApiService {
 
+    private static final String API_KEY = "DEMO_KEY";
+
+
     public MarsRoverApiResponse getRoverData(String roverType) {
 
         RestTemplate rt = new RestTemplate();
 
         ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa" +
-                        ".gov/mars-photos/api/v1/rovers/"+roverType+"/photos?sol=2&api_key=DEMO_KEY",
+                        ".gov/mars-photos/api/v1/rovers/"+roverType+"/photos?sol=2&api_key=" + API_KEY,
                 MarsRoverApiResponse.class);
         return response.getBody();
     }
--- src/main/java/com/earlyobject/web/HomeController.java
+++ src/main/java/com/earlyobject/web/HomeController.java
@@ -5,8 +5,8 @@ import com.earlyobject.service.MarsRoverApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,20 +16,14 @@ public class HomeController {
     private MarsRoverApiService roverService;
 
     @GetMapping("/")
-    public String getHomeView (ModelMap model) {
-        MarsRoverApiResponse roverData = roverService.getRoverData("opportunity");
-        model.put("roverData", roverData);
-
-        return "index";
-    }
-
-    @PostMapping("/")
-    public String postHomeView(ModelMap model, @RequestParam String marsApiRoverData) {
+    public String getHomeView (ModelMap model, @RequestParam(required = false) String marsApiRoverData) {
+        if (StringUtils.isEmpty(marsApiRoverData)) {
+            marsApiRoverData = "opportunity";
+        }
         MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData);
         model.put("roverData", roverData);
 
         return "index";
     }
-
 
 }
